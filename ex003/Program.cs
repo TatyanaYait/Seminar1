adfac21edbea269bc5eb432817384c9f0c5a0ab1
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-19 00:43:20 +0600

    квадрат числа или нет

---
+++ ex003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a == b * b)
+    Console.WriteLine("Да");
+else
+
+    Console.WriteLine("Нет");
+
