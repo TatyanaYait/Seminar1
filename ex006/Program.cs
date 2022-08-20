01e2461c7bc93a4f7aed6cc5598f1e7045ec949c
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-20 22:09:04 +0600

    Вывод дня недели

---
+++ ex006/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите число от 1 до 7: ");
+int A = int.Parse(Console.ReadLine());
+if (A == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+else if (A == 2)
+{
+    Console.WriteLine("Вторник");
+}
+else if (A == 3)
+{
+    Console.WriteLine("Среда");
+}
+else if (A == 4)
+{
+    Console.WriteLine("Четверг");
+}
+else if (A == 5)
+{
+    Console.WriteLine("Пятница");
+}
+else if (A == 6)
+{
+    Console.WriteLine("Суббота");
+}
+else
+    Console.WriteLine("Воскресенье");
