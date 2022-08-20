6fa04e12431e6be93051a1c2c8404c7ecb2d5b89
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-20 23:49:08 +0600

    Проверка числа на четность

---
+++ HomeWork__6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int b;
+
+b = a % 2;
+
+if (b == 0)
+{
+    Console.Write("это четное число");
+}
+else 
+    Console.Write("это нечетное число");
